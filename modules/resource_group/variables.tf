6cabf4beaeb0385ecce43727eb08dbed630c172a
codeuday95 <86396887+codeuday95@users.noreply.github.com>
2026-04-02 16:38:03 +0000

    Add resource group variables for Terraform

---
+++ modules/resource_group/variables.tf
@@ -0,0 +1,11 @@
+# Resource Group Variables
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the resource group"
+  type        = string
+}
